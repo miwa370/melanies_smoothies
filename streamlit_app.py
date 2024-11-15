8bbd25d32d0ecf8a6ea0378de5370d6a01035796
miwa370 <hashimoto.miwa.5063@members.co.jp>
2024-11-15 16:36:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 #Conbert the Snowflake Dataframe to a  Pandas Dataframe sp we can the LOC function
-pd_df = my.dataframe.to_pandas()
+pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
